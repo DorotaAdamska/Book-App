1a62e6be6140b1a7b45db3c388bc0ce1f4a166f3
Dorota Adamska <dadamska@consorg.pl>
2021-12-26 21:11:23 +0100

    add unsaved files

--- books-app/src/interfaces.tsx
+++ books-app/src/interfaces.tsx
@@ -1,13 +1,3 @@
-export interface PropsBooks {
-    books: Book[],
-    paramTwo: string,
-    optionalParam?: string
-  }
-
-  export interface PropsBook {
-    book: Book
-  }
-
 export interface Book {
     id: string,
     title: string,
